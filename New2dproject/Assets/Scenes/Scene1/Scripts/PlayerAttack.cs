9b346cd64d123c0a377971543a80c4a444ab9ae0
Dustin Stanley <dustin.stanley@gmail.com>
2013-12-08 22:05:20 -0800

    Support for multiple weapons

--- New2dproject/Assets/Scenes/Scene1/Scripts/PlayerAttack.cs
+++ New2dproject/Assets/Scenes/Scene1/Scripts/PlayerAttack.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class PlayerAttack : MonoBehaviour {
 
 	public Rigidbody bulletPrefab;
-	private float bulletSpeed = 10.0f;
 
 	// Use this for initialization
 
@@ -16,22 +15,7 @@ public class PlayerAttack : MonoBehaviour {
 	}
 
 	void bulletAttack() {
-		Rigidbody instantiatedProjectile =  (Rigidbody)Instantiate(bulletPrefab, transform.position, transform.rotation);
-		
-		// get target in world space, at same distance from camera:
-		Vector3 targetScreenPos = Input.mousePosition;
-		var targetPosition = Camera.main.ScreenToWorldPoint(targetScreenPos);
-		
-		// calculate direction & velocity:
-		Vector3 targetDelta = (targetPosition - transform.position);
-		targetDelta.z = 0;	//we dont care about the z-axis in a 2d game
-		Vector3 launchVelocity = targetDelta.normalized * bulletSpeed;	//Vector3.normalized returns a read-only copy of the vector with a magnitude of 1
-		print ("targetDelta.normalized = " + targetDelta.normalized.ToString());
-		print ("launchVelocity = " + launchVelocity.ToString());
-
-
-		instantiatedProjectile.velocity = launchVelocity;
-//		instantiatedProjectile.transform.LookAt(transform.position);
-//		instantiatedProjectile.AddForce(instantiatedProjectile.transform.forward * bulletSpeed);
+		WeaponBehavior behavior = WeaponBehaviorFactory.GetWeaponBehavior(WeaponBehaviorEnum.PISTOL);
+		behavior.FireWeapon(this, transform, bulletPrefab);
 	}
 }
