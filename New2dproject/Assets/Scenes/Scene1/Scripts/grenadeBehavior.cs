a199a9324c938999711bc77a9e99e8052633cb40
jklre <jklre2001@yahoo.com>
2013-12-10 07:48:28 -0800

    grenade fixes

---
+++ New2dproject/Assets/Scenes/Scene1/Scripts/grenadeBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grenadeBehavior : MonoBehaviour {
+
+	public AudioClip explode;
+
+	void Start () {
+		Object.Destroy(gameObject, 3.5f);
+
+		audio.Play();
+		 
+	}
+
+}
