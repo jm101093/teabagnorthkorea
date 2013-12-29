7ad3c5f6eabb7f084e7447aab1f5f58261b202cc
jklre <jklre2001@yahoo.com>
2013-12-28 18:16:46 -0800

    paralax background

---
+++ New2dproject/Assets/Scenes/Scene1/Scripts/paralaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paralaxBackground : MonoBehaviour {
+
+//	public float orgPos;
+//	public int offset;
+//	public bool FollowCamera;
+
+	// Use this for initialization
+	void Start () {
+//		orgPos = Camera.main.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (FollowCamera){ 
+//			Transform.position.x = (Camera.main.transform.position.x - orgPos)/offset;
+//		}
+//		else{
+//			Transform.position.x = (orgPos - Camera.main.transform.position.x)/offset;
+//			}
+		}
+	}
+
---
+++ New2dproject/Assets/Scenes/Scene1/Scripts/paralaxJAva.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var orgPos : float;
+var offset : int;
+var FollowCamera : boolean;
+
+function Start () {
+	orgPos = Camera.main.transform.position.x;
+}
+
+function Update () {
+	if (FollowCamera){ 
+			transform.position.x = (Camera.main.transform.position.x - orgPos)/offset;
+		}
+	else{
+			transform.position.x = (orgPos - Camera.main.transform.position.x)/offset;
+		}
+
+}
